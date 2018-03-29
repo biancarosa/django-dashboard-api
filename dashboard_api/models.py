dca590c0e78872f12fb1299f442aaa26eb591713
Bianca Rosa <me@biancarosa.com.br>
2018-03-29 11:22:49 -0300

    make better imports

--- dashboard_api/models.py
+++ dashboard_api/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 from django.contrib.auth.models import User
-from django.db import models as django_models
+from django import db
 
-class Settings(django_models.Model):
+class Settings(db.models.Model):
     COLORS = (
         (1, 'red'),
         (2, 'blue'),
         (3, 'green')
     )
     user = models.OneToOneField(User, on_delete=models.CASCADE) 
-    preferred_color = django_models.IntegerField(choices=COLORS)
+    preferred_color = db.models.IntegerField(choices=COLORS)
